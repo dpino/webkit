1585b1218381c9e195e95bbec0a0e7feac053ff3
Pawel Lampe <plampe@igalia.com>
2024-06-20 13:40:20 +0200

    Fallback to full-layout happens when more than 1 subtree layout is pending https://bugs.webkit.org/show_bug.cgi?id=275394
    Reviewed by NOBODY (OOPS!).
    
    Originally, subtree layout was designed to work with a single subtree. However,
    there are use-cases which require subtree layout to support multiple subtrees at once
    so that no fallback to "full" layout is done.
    
    One of such use-cases is CSS containment, which (under certain conditions)
    allows one to isolate parts of DOM tree so that in case of internal changes they can
    be laid out without layouting full tree. With CSS containment, it's fairly common that
    such internal subtree changes will happen at once thus leading to multiple subtree layouts
    being scheduled. Unfortunately, currenly such situation leads to fallback to "full" layout
    as only single subtree layout is supported. Fortunately, currently it's possible (although
    not ideal) to workaround that problem by forcing layout after each subtree change using
    position queries such as document.body.offsetLeft etc.
    
    This PR adds support for layouting multiple subtrees at once. This is achived
    by collecting pending subtrees and eventually layouting them one by one.
    
    * PerformanceTests/Containment/multiple-changes-in-large-grid.html: Added.
    * Source/WebCore/dom/Document.cpp:
    (WebCore::Document::updateLayoutIfDimensionsOutOfDate):
    * Source/WebCore/page/LocalFrameView.cpp:
    (WebCore::LocalFrameView::willDestroyRenderTree):
    (WebCore::LocalFrameView::didDestroyRenderTree):
    (WebCore::LocalFrameView::calculateScrollbarModesForLayout):
    (WebCore::LocalFrameView::autoSizeIfEnabled):
    (WebCore::LocalFrameView::forceLayout):
    * Source/WebCore/page/LocalFrameViewLayoutContext.cpp:
    (WebCore::LocalFrameViewLayoutContext::layout):
    (WebCore::LocalFrameViewLayoutContext::performLayouts):
    (WebCore::LocalFrameViewLayoutContext::performLayout):
    (WebCore::LocalFrameViewLayoutContext::reset):
    (WebCore::LocalFrameViewLayoutContext::needsLayoutInternal const):
    (WebCore::LocalFrameViewLayoutContext::scheduleLayout):
    (WebCore::LocalFrameViewLayoutContext::scheduleSubtreeLayout):
    (WebCore::LocalFrameViewLayoutContext::hasSubtreeLayoutRoot const):
    (WebCore::LocalFrameViewLayoutContext::clearSubtreeLayoutRoots):
    (WebCore::LocalFrameViewLayoutContext::convertSubtreeLayoutToFullLayout):
    (WebCore::LocalFrameViewLayoutContext::addSubtreeLayoutRoot):
    (WebCore::LocalFrameViewLayoutContext::removeSubtreeLayoutRoot):
    (WebCore::LocalFrameViewLayoutContext::canPerformLayout const):
    (WebCore::LocalFrameViewLayoutContext::subtreeLayoutRoot const): Deleted.
    (WebCore::LocalFrameViewLayoutContext::setSubtreeLayoutRoot): Deleted.
    * Source/WebCore/page/LocalFrameViewLayoutContext.h:
    * Source/WebCore/rendering/RenderBox.cpp:
    (WebCore::RenderBox::updateFloatPainterAfterSelfPaintingLayerChange):
    * Source/WebCore/rendering/RenderElement.cpp:
    (WebCore::RenderElement::clearSubtreeLayoutRootIfNeeded const):

--- Source/WebCore/page/LocalFrameView.cpp
+++ Source/WebCore/page/LocalFrameView.cpp
@@ -520,12 +520,12 @@ void LocalFrameView::didRestoreFromBackForwardCache()
 void LocalFrameView::willDestroyRenderTree()
 {
     detachCustomScrollbars();
-    layoutContext().clearSubtreeLayoutRoot();
+    layoutContext().clearSubtreeLayoutRoots();
 }
 
 void LocalFrameView::didDestroyRenderTree()
 {
-    ASSERT(!layoutContext().subtreeLayoutRoot());
+    ASSERT(!layoutContext().isSubtreeLayout());
     ASSERT(m_widgetsInRenderTree.isEmpty());
 
     ASSERT(!m_embeddedObjectsToUpdate || m_embeddedObjectsToUpdate->isEmpty());
@@ -704,7 +704,7 @@ void LocalFrameView::calculateScrollbarModesForLayout(ScrollbarMode& hMode, Scro
         vMode = ScrollbarMode::AlwaysOff;
     }
     
-    if (layoutContext().subtreeLayoutRoot())
+    if (layoutContext().isSubtreeLayout())
         return;
     
     auto* document = m_frame->document();
@@ -4444,7 +4444,7 @@ void LocalFrameView::autoSizeIfEnabled()
         return;
 
     SetForScope changeInAutoSize(m_inAutoSize, true);
-    if (layoutContext().subtreeLayoutRoot())
+    if (layoutContext().isSubtreeLayout())
         layoutContext().convertSubtreeLayoutToFullLayout();
 
     switch (m_autoSizeMode) {
@@ -5655,7 +5655,7 @@ void LocalFrameView::enableAutoSizeMode(bool enable, const IntSize& viewSize, Au
 
 void LocalFrameView::forceLayout(bool allowSubtreeLayout)
 {
-    if (!allowSubtreeLayout && layoutContext().subtreeLayoutRoot())
+    if (!allowSubtreeLayout && layoutContext().isSubtreeLayout())
         layoutContext().convertSubtreeLayoutToFullLayout();
     layoutContext().layout();
 }
