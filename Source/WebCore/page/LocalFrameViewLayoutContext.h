15e7b23cbd72e7a6cc2a5b73999c8ef5be08a4da
Pawel Lampe <plampe@igalia.com>
2024-06-20 13:40:20 +0200

    Fallback to full-layout happens when more than 1 subtree layout is pending https://bugs.webkit.org/show_bug.cgi?id=275394
    Reviewed by NOBODY (OOPS!).
    
    Originally, subtree layout was designed to work with a single subtree. However,
    there are use-cases which require subtree layout to support multiple subtrees at once
    so that no fallback to "full" layout is done.
    
    One of such use-cases is CSS containment, which (under certain conditions)
    allows one to isolate parts of DOM tree so that in case of internal changes they can
    be laid out without layouting full tree. With CSS containment, it's fairly common that
    such internal subtree changes will happen at once thus leading to multiple subtree layouts
    being scheduled. Unfortunately, currenly such situation leads to fallback to "full" layout
    as only single subtree layout is supported. Fortunately, currently it's possible (although
    not ideal) to workaround that problem by forcing layout after each subtree change using
    position queries such as document.body.offsetLeft etc.
    
    This PR adds support for layouting multiple subtrees at once. This is achived
    by collecting pending subtrees and eventually layouting them one by one.
    
    * PerformanceTests/Containment/multiple-changes-in-large-grid.html: Added.
    * Source/WebCore/dom/Document.cpp:
    (WebCore::Document::updateLayoutIfDimensionsOutOfDate):
    * Source/WebCore/page/LocalFrameView.cpp:
    (WebCore::LocalFrameView::willDestroyRenderTree):
    (WebCore::LocalFrameView::didDestroyRenderTree):
    (WebCore::LocalFrameView::calculateScrollbarModesForLayout):
    (WebCore::LocalFrameView::autoSizeIfEnabled):
    (WebCore::LocalFrameView::forceLayout):
    * Source/WebCore/page/LocalFrameViewLayoutContext.cpp:
    (WebCore::LocalFrameViewLayoutContext::layout):
    (WebCore::LocalFrameViewLayoutContext::performLayouts):
    (WebCore::LocalFrameViewLayoutContext::performLayout):
    (WebCore::LocalFrameViewLayoutContext::reset):
    (WebCore::LocalFrameViewLayoutContext::needsLayoutInternal const):
    (WebCore::LocalFrameViewLayoutContext::scheduleLayout):
    (WebCore::LocalFrameViewLayoutContext::scheduleSubtreeLayout):
    (WebCore::LocalFrameViewLayoutContext::hasSubtreeLayoutRoot const):
    (WebCore::LocalFrameViewLayoutContext::clearSubtreeLayoutRoots):
    (WebCore::LocalFrameViewLayoutContext::convertSubtreeLayoutToFullLayout):
    (WebCore::LocalFrameViewLayoutContext::addSubtreeLayoutRoot):
    (WebCore::LocalFrameViewLayoutContext::removeSubtreeLayoutRoot):
    (WebCore::LocalFrameViewLayoutContext::canPerformLayout const):
    (WebCore::LocalFrameViewLayoutContext::subtreeLayoutRoot const): Deleted.
    (WebCore::LocalFrameViewLayoutContext::setSubtreeLayoutRoot): Deleted.
    * Source/WebCore/page/LocalFrameViewLayoutContext.h:
    * Source/WebCore/rendering/RenderBox.cpp:
    (WebCore::RenderBox::updateFloatPainterAfterSelfPaintingLayerChange):
    * Source/WebCore/rendering/RenderElement.cpp:
    (WebCore::RenderElement::clearSubtreeLayoutRootIfNeeded const):

--- Source/WebCore/page/LocalFrameViewLayoutContext.h
+++ Source/WebCore/page/LocalFrameViewLayoutContext.h
@@ -94,6 +94,7 @@ public:
     LayoutPhase layoutPhase() const { return m_layoutPhase; }
     bool isLayoutNested() const { return m_layoutNestedState == LayoutNestedState::Nested; }
     bool isLayoutPending() const { return m_layoutTimer.isActive(); }
+    bool isSubtreeLayout() const { return !m_subtreeLayoutRoots.isEmpty(); }
     bool isInLayout() const { return layoutPhase() != LayoutPhase::OutsideLayout; }
     bool isInRenderTreeLayout() const { return layoutPhase() == LayoutPhase::InRenderTreeLayout; }
     bool inPaintableState() const { return layoutPhase() != LayoutPhase::InRenderTreeLayout && layoutPhase() != LayoutPhase::InViewSizeAdjust && (layoutPhase() != LayoutPhase::InPostLayout || inAsynchronousTasks()); }
@@ -110,8 +111,9 @@ public:
     std::optional<TextBoxTrim> textBoxTrim() const { return m_textBoxTrim; }
     void setTextBoxTrim(std::optional<TextBoxTrim> textBoxTrim) { m_textBoxTrim = textBoxTrim; }
 
-    RenderElement* subtreeLayoutRoot() const;
-    void clearSubtreeLayoutRoot() { m_subtreeLayoutRoot.clear(); }
+    bool hasSubtreeLayoutRoot(const RenderElement&) const;
+    void removeSubtreeLayoutRoot(const RenderElement&);
+    void clearSubtreeLayoutRoots();
     void convertSubtreeLayoutToFullLayout();
 
     void reset();
@@ -171,7 +173,8 @@ private:
 
     bool needsLayoutInternal() const;
 
-    void performLayout(bool canDeferUpdateLayerPositions);
+    void performLayouts(bool canDeferUpdateLayerPositions);
+    bool performLayout(bool canDeferUpdateLayerPositions, bool unchecked = false);
     bool canPerformLayout() const;
     bool isLayoutSchedulingEnabled() const { return m_layoutSchedulingIsEnabled; }
 
@@ -182,7 +185,7 @@ private:
     void runOrScheduleAsynchronousTasks(bool canDeferUpdateLayerPositions);
     bool inAsynchronousTasks() const { return m_inAsynchronousTasks; }
 
-    void setSubtreeLayoutRoot(RenderElement&);
+    void addSubtreeLayoutRoot(RenderElement&);
 
 #if ENABLE(TEXT_AUTOSIZING)
     void applyTextSizingIfNeeded(RenderElement& layoutRoot);
@@ -223,7 +226,7 @@ private:
     SingleThreadWeakRef<LocalFrameView> m_frameView;
     Timer m_layoutTimer;
     Timer m_postLayoutTaskTimer;
-    SingleThreadWeakPtr<RenderElement> m_subtreeLayoutRoot;
+    HashSet<RenderElement*> m_subtreeLayoutRoots;
 
     bool m_layoutSchedulingIsEnabled { true };
     bool m_firstLayout { true };
