336e46cd37be550041b52632fe5d69c146250fda
Diego Pino Garcia <dpino@igalia.com>
2024-08-28 07:24:56 +0200

    Resolve conflicts

--- Source/WebKit/WebProcess/WebPage/WebPage.h
+++ Source/WebKit/WebProcess/WebPage/WebPage.h
@@ -2731,14 +2731,8 @@ private:
     bool m_isAppNapEnabled { true };
     UserActivity m_userActivity;
 
-<<<<<<< HEAD
     Markable<WebCore::NavigationIdentifier> m_pendingNavigationID;
-||||||| parent of 783505fe73dd (chore(webkit/): bootstrap build #2066)
-    WebCore::NavigationIdentifier m_pendingNavigationID;
-=======
-    WebCore::NavigationIdentifier m_pendingNavigationID;
-    WebCore::NavigationIdentifier m_pendingFrameNavigationID;
->>>>>>> 783505fe73dd (chore(webkit/): bootstrap build #2066)
+    Markable<WebCore::NavigationIdentifier> m_pendingFrameNavigationID;
     std::optional<WebsitePoliciesData> m_pendingWebsitePolicies;
 
     bool m_mainFrameProgressCompleted { false };
