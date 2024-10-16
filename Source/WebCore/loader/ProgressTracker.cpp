b0c8c75510313383f68270633f8f2b1acab2754d
playwright-devops <devops@playwright.dev>
2024-10-16 08:48:37 +0200

    chore(webkit): bootstrap build #2092

--- Source/WebCore/loader/ProgressTracker.cpp
+++ Source/WebCore/loader/ProgressTracker.cpp
@@ -161,6 +161,8 @@ void ProgressTracker::progressCompleted(LocalFrame& frame)
     if (!m_numProgressTrackedFrames || m_originatingProgressFrame == &frame)
         finalProgressComplete();
 
+    InspectorInstrumentation::frameStoppedLoading(frame);
+
     m_client->didChangeEstimatedProgress();
 }
 
@@ -186,9 +188,17 @@ void ProgressTracker::finalProgressComplete()
     frame->protectedLoader()->protectedClient()->setMainFrameDocumentReady(true);
     m_client->progressFinished(*frame);
     protectedPage()->progressFinished(*frame);
+<<<<<<< HEAD
     frame->protectedLoader()->loadProgressingStatusChanged();
 
     InspectorInstrumentation::frameStoppedLoading(*frame);
+||||||| parent of 7adc6076b9b3 (chore(webkit): bootstrap build #2092)
+    frame->checkedLoader()->loadProgressingStatusChanged();
+
+    InspectorInstrumentation::frameStoppedLoading(*frame);
+=======
+    frame->checkedLoader()->loadProgressingStatusChanged();
+>>>>>>> 7adc6076b9b3 (chore(webkit): bootstrap build #2092)
 }
 
 void ProgressTracker::incrementProgress(ResourceLoaderIdentifier identifier, const ResourceResponse& response)
