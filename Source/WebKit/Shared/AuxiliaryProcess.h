102667c6103d09f4bb216f5030206f99ba9ca640
playwright-devops <devops@playwright.dev>
2024-11-29 20:47:12 +0100

    chore(webkit/): bootstrap build #2113

--- Source/WebKit/Shared/AuxiliaryProcess.h
+++ Source/WebKit/Shared/AuxiliaryProcess.h
@@ -211,6 +211,11 @@ struct AuxiliaryProcessInitializationParameters {
     IPC::Connection::Identifier connectionIdentifier;
     HashMap<String, String> extraInitializationData;
     WTF::AuxiliaryProcessType processType;
+// Playwright begin
+#if !PLATFORM(COCOA)
+    bool shouldEnableSharedArrayBuffer { false };
+#endif
+// Playwright end
 };
 
 } // namespace WebKit
