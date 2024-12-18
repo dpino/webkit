6063de5a94782c0aba4e64040e24a03fe5fb1e96
Alejandro G. Castro <alex@igalia.com>
2024-12-18 21:28:39 +0100

    [PATCH] ThreadedCompositor: Avoid calling glClear in each composition.
    glClear is very slow in the iMX.8 GPU, in this patch we add 2 options:
    
       - For easy testing, avoid any clear using WEBKIT_STOP_CLEAR_COMPOSITING
         set to 1.
       - To get a better performance and keep doing the clear use a shader
         using the variable WEBKIT_FORCE_SHADER_CLEAR_COMPOSITING set to 1.
    
    If WEBKIT_STOP_CLEAR_COMPOSITING is set no clear happens even if you
    define WEBKIT_FORCE_SHADER_CLEAR_COMPOSITING.
    
    This patch is just meaningful for the iMX.8 problem with the glClear
    performance.
    
    Original-Author: Lauro Moura <lmoura@igalia.com>
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>

--- Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AcceleratedSurface.h
+++ Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AcceleratedSurface.h
@@ -126,9 +126,11 @@ public:
     SkCanvas* canvas();
 
     void willDestroyGLContext();
+    void didCreateGLContext();
     void willRenderFrame(const WebCore::IntSize&);
     void didRenderFrame();
     void sendFrame();
+    void doClear(float, float, float, float);
     void clear(const OptionSet<WebCore::CompositionReason>&);
 
 #if ENABLE(DAMAGE_TRACKING)
@@ -437,6 +439,8 @@ private:
     };
 #endif
 
+    static void checkClearShader();
+
     const WeakRef<WebPage> m_webPage;
     Function<void()> m_frameCompleteHandler;
     bool m_useSkia { false };
@@ -456,6 +460,14 @@ private:
 #if ENABLE(DAMAGE_TRACKING)
     SwapChainDamageTracker m_damageTracker;
 #endif
+
+    guint m_clearProgram;
+    guint m_clearColorUniformLocation;
+    guint m_vertexShader;
+    guint m_fragmentShader;
+    guint m_vao;
+    guint m_vbo;
+    static bool m_force_shader_clear;
 };
 
 } // namespace WebKit
