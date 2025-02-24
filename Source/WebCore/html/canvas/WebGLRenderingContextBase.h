e61be40b984c1375f56c4ac6c51ca52fb0e97cb7
Pawel Lampe <plampe@igalia.com>
2025-02-24 11:32:21 +0100

    Improve the preserveDrawingBuffer: true performance using damage Need the bug URL (OOPS!).
    Reviewed by NOBODY (OOPS!).
    
    Explanation of why this fixes the bug (OOPS!).
    
    * Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp:
    (WebCore::WebGLRenderingContextBase::disable):
    (WebCore::WebGLRenderingContextBase::scissor):
    (WebCore::WebGLRenderingContextBase::prepareForDisplay):
    (WebCore::WebGLRenderingContextBase::clearAccumulatedDirtyRect):
    * Source/WebCore/html/canvas/WebGLRenderingContextBase.h:
    * Source/WebCore/platform/graphics/GraphicsContextGL.h:
    (WebCore::GraphicsContextGL::setDamage):
    * Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.cpp:
    (WebCore::GraphicsContextGLANGLE::prepareTexture):
    (WebCore::GraphicsContextGLANGLE::setDamage):
    * Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.h:

--- Source/WebCore/html/canvas/WebGLRenderingContextBase.h
+++ Source/WebCore/html/canvas/WebGLRenderingContextBase.h
@@ -495,6 +495,8 @@ public:
 
     bool compositingResultsNeedUpdating() const final { return m_compositingResultsNeedUpdating; }
     void prepareForDisplay() final;
+
+    void clearAccumulatedDirtyRect() final;
 protected:
     WebGLRenderingContextBase(CanvasBase&, CanvasRenderingContext::Type, WebGLContextAttributes&&);
 
@@ -1061,6 +1063,8 @@ private:
     // The ordinal number of when the context was last active (drew, read pixels).
     uint64_t m_activeOrdinal { 0 };
     WeakPtrFactory<WebGLRenderingContextBase> m_contextObjectWeakPtrFactory;
+    std::optional<FloatRect> m_latestScissor;
+    std::optional<FloatRect> m_dirtyRect;
 };
 
 template<typename T>
