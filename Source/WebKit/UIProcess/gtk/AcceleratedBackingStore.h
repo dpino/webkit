56116dd5eff5e41a6c60ec060a9d34d6c7de8189
playwright-devops <devops@playwright.dev>
2025-07-29 15:45:38 +0200

    chore(webkit): bootstrap build #2199

--- Source/WebKit/UIProcess/gtk/AcceleratedBackingStore.h
+++ Source/WebKit/UIProcess/gtk/AcceleratedBackingStore.h
@@ -40,7 +40,14 @@
 #include <wtf/glib/GRefPtr.h>
 #include <wtf/unix/UnixFileDescriptor.h>
 
+<<<<<<< HEAD
 typedef void *EGLImage;
+||||||| parent of 6685731cc01c (chore(webkit): bootstrap build #2199)
+typedef struct _cairo cairo_t;
+=======
+typedef struct _cairo cairo_t;
+typedef struct _cairo_surface cairo_surface_t;
+>>>>>>> 6685731cc01c (chore(webkit): bootstrap build #2199)
 
 #if USE(GBM)
 struct gbm_bo;
@@ -68,8 +75,24 @@ public:
 #if USE(GBM)
     static Vector<RendererBufferFormat> preferredBufferFormats();
 #endif
+<<<<<<< HEAD
     static RefPtr<AcceleratedBackingStore> create(WebPageProxy&);
     ~AcceleratedBackingStore();
+||||||| parent of 6685731cc01c (chore(webkit): bootstrap build #2199)
+    virtual void realize() { };
+    virtual void unrealize() { };
+    virtual int renderHostFileDescriptor() { return -1; }
+    virtual RendererBufferFormat bufferFormat() const { return { }; }
+    virtual RefPtr<WebCore::NativeImage> bufferAsNativeImageForTesting() const = 0;
+=======
+    virtual cairo_surface_t* surface() { return nullptr; }
+
+    virtual void realize() { };
+    virtual void unrealize() { };
+    virtual int renderHostFileDescriptor() { return -1; }
+    virtual RendererBufferFormat bufferFormat() const { return { }; }
+    virtual RefPtr<WebCore::NativeImage> bufferAsNativeImageForTesting() const = 0;
+>>>>>>> 6685731cc01c (chore(webkit): bootstrap build #2199)
 
     void ref() const final { RefCounted::ref(); }
     void deref() const final { RefCounted::deref(); }
