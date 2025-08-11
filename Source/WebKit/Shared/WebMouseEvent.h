3ee3bdc9c5f1aaa47f103200dc08caf9f9221240
playwright-devops <devops@playwright.dev>
2025-08-11 19:47:16 +0200

    chore(webkit): bootstrap build #2201

--- Source/WebKit/Shared/WebMouseEvent.h
+++ Source/WebKit/Shared/WebMouseEvent.h
@@ -70,9 +70,20 @@ public:
 
     WebMouseEventButton button() const { return m_button; }
     unsigned short buttons() const { return m_buttons; }
+<<<<<<< HEAD
     const WebCore::DoublePoint& position() const { return m_position; } // Relative to the view.
     void setPosition(const WebCore::DoublePoint& position) { m_position = position; }
     const WebCore::DoublePoint& globalPosition() const { return m_globalPosition; }
+||||||| parent of a647ce92ba4a (chore(webkit): bootstrap build #2201)
+    const WebCore::IntPoint& position() const { return m_position; } // Relative to the view.
+    void setPosition(const WebCore::IntPoint& position) { m_position = position; }
+    const WebCore::IntPoint& globalPosition() const { return m_globalPosition; }
+=======
+    void playwrightSetButtons(unsigned short buttons) { m_buttons = buttons; }
+    const WebCore::IntPoint& position() const { return m_position; } // Relative to the view.
+    void setPosition(const WebCore::IntPoint& position) { m_position = position; }
+    const WebCore::IntPoint& globalPosition() const { return m_globalPosition; }
+>>>>>>> a647ce92ba4a (chore(webkit): bootstrap build #2201)
     float deltaX() const { return m_deltaX; }
     float deltaY() const { return m_deltaY; }
     float deltaZ() const { return m_deltaZ; }
