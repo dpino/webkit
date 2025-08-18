ba1854d62e55f849d440e15939e0b249b3e7c260
Diego Pino Garcia <dpino@igalia.com>
2025-08-18 14:46:21 +0200

    Resolve conflicts

--- Source/WebCore/Modules/geolocation/Geolocation.cpp
+++ Source/WebCore/Modules/geolocation/Geolocation.cpp
@@ -376,13 +376,7 @@ bool Geolocation::shouldBlockGeolocationRequests()
     bool isLocalOrigin = securityOrigin()->isLocal();
     bool isPotentiallyTrustworthy = securityOrigin()->isPotentiallyTrustworthy();
     if (document->canAccessResource(ScriptExecutionContext::ResourceType::Geolocation) != ScriptExecutionContext::HasResourceAccess::No) {
-<<<<<<< HEAD
-        if (isLocalOrigin || isSecure)
-||||||| parent of d39a7328bbd8 (chore(webkit): bootstrap build #2203)
-        if (isLocalOrigin || (isSecure && !hasMixedContent))
-=======
-        if (isLocalOrigin || (isSecure && !hasMixedContent) || isPotentiallyTrustworthy)
->>>>>>> d39a7328bbd8 (chore(webkit): bootstrap build #2203)
+        if (isLocalOrigin || isSecure  || isPotentiallyTrustworthy)
             return false;
     }
 
