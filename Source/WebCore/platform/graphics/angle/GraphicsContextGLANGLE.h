66d22f33f1863bc543ad3935cb96f18d1c870912
Pawel Lampe <plampe@igalia.com>
2025-09-10 13:25:06 +0200

    [PATCH] Improve WebGL performance using Damage

--- Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.h
+++ Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.h
@@ -29,6 +29,7 @@
 #if ENABLE(WEBGL)
 
 #include <WebCore/ANGLEUtilities.h>
+#include <WebCore/Damage.h>
 #include <WebCore/GCGLSpan.h>
 #include <WebCore/GraphicsContextGL.h>
 #include <WebCore/GraphicsContextGLState.h>
@@ -353,6 +354,8 @@ public:
 
     RefPtr<NativeImage> copyNativeImage(SurfaceBuffer) override;
 
+    void setDamage(Damage&&) final;
+
     // Returns the span of valid data read on success.
     bool getBufferSubDataWithStatus(GCGLenum target, GCGLintptr offset, std::span<uint8_t> data);
 
@@ -459,6 +462,8 @@ protected:
     HashMap<uint32_t, void*, IntHash<uint32_t>, WTF::UnsignedWithZeroKeyHashTraits<uint32_t>> m_eglSyncs;
     IntSize m_maxInternalFramebufferSize;
     bool m_useBlitFallback { false };
+    std::optional<Damage> m_damage;
+    std::optional<Damage> m_previousDamage;
 };
 
 
