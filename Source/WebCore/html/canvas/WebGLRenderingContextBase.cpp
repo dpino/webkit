c0f4cc6b60fe85798a39e05315301c24bfa2a0ba
Pawel Lampe <plampe@igalia.com>
2025-09-10 13:25:06 +0200

    Improve WebGL performance using Damage

--- Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
+++ Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
@@ -891,6 +891,10 @@ void WebGLRenderingContextBase::didUpdateCanvasSizeProperties(bool)
 
     m_readDrawingBuffer = nullptr;
     m_readDisplayBuffer = nullptr;
+    m_damage = std::nullopt;
+
+    // We don't have to mark the canvas as dirty, since the newly created image buffer will also start off
+    // clear (and this matches what reshape will do).
     m_defaultFramebuffer->reshape(newSize);
     updateMemoryCost();
 
@@ -1585,8 +1589,10 @@ void WebGLRenderingContextBase::disable(GCGLenum cap)
 {
     if (isContextLost() || !validateCapability("disable"_s, cap))
         return;
-    if (cap == GraphicsContextGL::SCISSOR_TEST)
+    if (cap == GraphicsContextGL::SCISSOR_TEST) {
         m_scissorEnabled = false;
+        m_damage = std::nullopt;
+    }
     if (cap == GraphicsContextGL::RASTERIZER_DISCARD)
         m_rasterizerDiscardEnabled = false;
     graphicsContextGL()->disable(cap);
@@ -3100,6 +3106,9 @@ void WebGLRenderingContextBase::scissor(GCGLint x, GCGLint y, GCGLsizei width, G
     if (!validateSize("scissor"_s, width, height))
         return;
     graphicsContextGL()->scissor(x, y, width, height);
+    m_latestScissor = { x, y, width, height };
+    if (m_scissorEnabled && m_damage)
+        m_damage->add(*m_latestScissor);
 }
 
 void WebGLRenderingContextBase::shaderSource(WebGLShader& shader, const String& string)
@@ -5658,6 +5667,11 @@ void WebGLRenderingContextBase::prepareForDisplay()
         return;
 
     clearIfComposited(CallerTypeOther);
+    if (m_damage) {
+        m_context->setDamage(WTF::move(*m_damage));
+        m_damage = std::nullopt;
+    }
+    clearAccumulatedDirtyRect();
     graphicsContextGL()->prepareForDisplay();
     m_defaultFramebuffer->markAllUnpreservedBuffersDirty();
 
@@ -5697,6 +5711,15 @@ void WebGLRenderingContextBase::updateMemoryCost() const
     size_t samplesPerPixel = m_attributes.antialias ? 4 : 1;
     newMemoryCost += area * samplesPerPixel * bytesPerSample;
     CanvasRenderingContext::updateMemoryCost(newMemoryCost);
+}
+
+void WebGLRenderingContextBase::clearAccumulatedDirtyRect()
+{
+    if (m_scissorEnabled && m_latestScissor) {
+        m_damage = std::make_optional<Damage>(clampedCanvasSize(), Damage::Mode::Rectangles, 5);
+        m_damage->add(*m_latestScissor);
+    } else
+        m_damage = std::nullopt;
 }
 
 WebCoreOpaqueRoot root(WebGLRenderingContextBase* context)
