25c2bc8ef85b8e28437ce504f084749c6abae2b8
playwright-devops <devops@playwright.dev>
2025-09-15 17:43:11 +0200

    chore(webkit): bootstrap build #2210

--- Source/WebCore/page/EventHandler.cpp
+++ Source/WebCore/page/EventHandler.cpp
@@ -4569,6 +4569,12 @@ bool EventHandler::handleDrag(const MouseEventWithHitTestResults& event, CheckDr
     if (!document)
         return false;
 
+#if PLATFORM(MAC)
+    auto* page = m_frame->page();
+    if (page && !page->overrideDragPasteboardName().isEmpty())
+        dragState().dataTransfer = DataTransfer::createForDrag(*document, page->overrideDragPasteboardName());
+    else
+#endif
     dragState().dataTransfer = DataTransfer::createForDrag(*document);
     auto hasNonDefaultPasteboardData = HasNonDefaultPasteboardData::No;
     
@@ -5173,6 +5179,7 @@ static HitTestResult hitTestResultInFrame(LocalFrame* frame, const LayoutPoint& 
     return result;
 }
 
+WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN
 Expected<bool, RemoteFrameGeometryTransformer> EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
 {
     Ref frame = m_frame.get();
@@ -5246,7 +5253,7 @@ Expected<bool, RemoteFrameGeometryTransformer> EventHandler::handleTouchEvent(co
 
         // Increment the platform touch id by 1 to avoid storing a key of 0 in the hashmap.
         unsigned touchPointTargetKey = point.id() + 1;
-#if PLATFORM(WPE)
+#if !ENABLE(IOS_TOUCH_EVENTS)
         bool pointerCancelled = false;
 #endif
         RefPtr<EventTarget> touchTarget;
@@ -5293,7 +5300,7 @@ Expected<bool, RemoteFrameGeometryTransformer> EventHandler::handleTouchEvent(co
             // we also remove it from the map.
             touchTarget = m_originatingTouchPointTargets.take(touchPointTargetKey);
 
-#if PLATFORM(WPE)
+#if !ENABLE(IOS_TOUCH_EVENTS)
             HitTestResult result = hitTestResultAtPoint(pagePoint, hitType | HitTestRequest::Type::AllowChildFrameContent);
             pointerTarget = result.targetElement();
             pointerCancelled = (pointerTarget != touchTarget);
@@ -5316,7 +5323,7 @@ Expected<bool, RemoteFrameGeometryTransformer> EventHandler::handleTouchEvent(co
         if (!targetFrame)
             continue;
 
-#if PLATFORM(WPE)
+#if !ENABLE(IOS_TOUCH_EVENTS)
         // FIXME: WPE currently does not send touch stationary events, so create a naive TouchReleased PlatformTouchPoint
         // on release if the hit test result changed since the previous TouchPressed or TouchMoved
         if (pointState == PlatformTouchPoint::TouchReleased && pointerCancelled) {
@@ -5403,6 +5410,7 @@ Expected<bool, RemoteFrameGeometryTransformer> EventHandler::handleTouchEvent(co
 
     return swallowedEvent;
 }
+WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN
 #endif // ENABLE(TOUCH_EVENTS) && !ENABLE(IOS_TOUCH_EVENTS)
 
 #if ENABLE(TOUCH_EVENTS)
