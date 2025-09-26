437990e66a6186da9721832edd19be6ab350fb2a
Yury Semikhatsky <yurys@chromium.org>
2025-09-26 14:23:54 -0700

    Destroy Frame target when WebFrameProxy disconnects from the page

--- Source/WebKit/UIProcess/WebFrameProxy.cpp
+++ Source/WebKit/UIProcess/WebFrameProxy.cpp
@@ -182,6 +182,9 @@ void WebFrameProxy::webProcessWillShutDown()
     for (auto& childFrame : std::exchange(m_childFrames, { }))
         childFrame->webProcessWillShutDown();
 
+    if (RefPtr page = m_page.get())
+        page->inspectorController().destroyInspectorTarget(WebFrameInspectorTarget::toTargetID(frameID()));
+
     m_page = nullptr;
 
     if (RefPtr activeListener = m_activeListener) {
