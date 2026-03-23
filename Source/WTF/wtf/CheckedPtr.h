fb2339a4aa208ccdb28726a58f108433b37f3c46
Chris Dumez <cdumez@apple.com>
2026-03-22 17:13:18 -0700

    Cherry-pick 309714@main (883de0d12b90). https://bugs.webkit.org/show_bug.cgi?id=310471
        IsSmartPtr::isNullable values are swapped between CheckedPtr and CheckedRef
        https://bugs.webkit.org/show_bug.cgi?id=310471
    
        Reviewed by Darin Adler.
    
        IsSmartPtr::isNullable drives the NullableSmartPtr / NonNullableSmartPtr
        concepts in GetPtr.h, which gate reference-taking convenience overloads
        in HashCountedSet.
    
        With the swap:
        - HashCountedSet<CheckedRef<T>> cannot use find(myObj) with a reference
          (the overload is gated on NonNullableSmartPtr, but CheckedRef
          incorrectly doesn't satisfy it)
        - HashCountedSet<CheckedPtr<T>> gets reference overloads it shouldn't have
    
        * Source/WTF/wtf/CheckedPtr.h:
        * Source/WTF/wtf/CheckedRef.h:
    
        Canonical link: https://commits.webkit.org/309714@main
    
    Canonical link: https://commits.webkit.org/305877.312@webkitglib/2.52

--- Source/WTF/wtf/CheckedPtr.h
+++ Source/WTF/wtf/CheckedPtr.h
@@ -201,7 +201,7 @@ struct GetPtrHelper<CheckedPtr<T, PtrTraits>> {
 template <typename T, typename U>
 struct IsSmartPtr<CheckedPtr<T, U>> {
     static constexpr bool value = true;
-    static constexpr bool isNullable = false;
+    static constexpr bool isNullable = true;
 };
 
 template<typename ExpectedType, typename ArgType, typename ArgPtrTraits>
