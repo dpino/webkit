9e70506e3cd6cc9db8285ac5251043c6964e6af1
Sihui Liu <sihui_liu@apple.com>
2026-03-25 08:07:35 -0700

    Cherry-pick 309914@main (7ca7678e5e18). https://bugs.webkit.org/show_bug.cgi?id=310425
        [ Debug ] Fix WKNavigation.HTTPSFirstWithHTTPRedirect
        https://bugs.webkit.org/show_bug.cgi?id=310425
        rdar://173051610
    
        Reviewed by Per Arne Vollan.
    
        The assertion `ASSERT(m_provisionalURL.isEmpty())` in `FrameLoadState::didStartProvisionalLoad()` fails when running the
        test. The cause is we do not set `FrameLoadState::m_provsionalURL` of `WebPageProxy::m_mainFrame` when the load of
        provisional frame fails, but we set it when the load of provisional frame starts or gets redirected. To fix this, this
        patch makes sure we also update the frame state in `WebPageProxy::didFailProvisionalLoadForFrameShared`, which matches
        the behavior in `WebPageProxy::didReceiveServerRedirectForProvisionalLoadForFrameShared` and
        `ProvisionalPageProxy::didStartProvisionalLoadForFrame`.
    
        * Source/WebKit/UIProcess/WebPageProxy.cpp:
        (WebKit::WebPageProxy::didFailProvisionalLoadForFrameShared):
    
        Canonical link: https://commits.webkit.org/309914@main

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -7468,6 +7468,12 @@ void WebPageProxy::didFailProvisionalLoadForFrameShared(Ref<WebProcessProxy>&& p
             navigation->setClientNavigationActivity(nullptr);
 
         callLoadCompletionHandlersIfNecessary(false);
+
+        // Update current main frame when provisional main frame load fails, because it
+        // is updated when provisional main frame load starts or gets redirected.
+        RefPtr mainFrame = m_mainFrame;
+        if (&frame != mainFrame && !mainFrame->frameLoadState().provisionalURL().isEmpty())
+            mainFrame->didFailProvisionalLoad();
     }
 
     frame.didFailProvisionalLoad();
