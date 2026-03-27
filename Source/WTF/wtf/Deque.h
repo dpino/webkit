d7ea5ce0b4875fd179ec6656a03e1443215f145a
Chris Dumez <cdumez@apple.com>
2026-03-27 09:21:16 -0700

    Cherry-pick 310082@main (7e3461d08115). https://bugs.webkit.org/show_bug.cgi?id=310873
        Deque's implementation truncates `size_t` to `unsigned` in a few places
        https://bugs.webkit.org/show_bug.cgi?id=310873
    
        Reviewed by Darin Adler.
    
        * Source/WTF/wtf/Deque.h:
    
        Canonical link: https://commits.webkit.org/310082@main
    
    Canonical link: https://commits.webkit.org/305877.387@webkitglib/2.52

--- Source/WTF/wtf/Deque.h
+++ Source/WTF/wtf/Deque.h
@@ -211,7 +211,7 @@ public:
     Iterator& operator--() { Base::decrement(); return *this; }
     // postfix -- intentionally omitted
 
-    // Only forwarding + unsigned is supported.
+    // Only forwarding + size_t is supported.
     Iterator& operator+=(size_t count) { Base::increment(count); return *this; }
     Iterator operator+(size_t count) const { Iterator result(*this); result += count; return result; }
 };
@@ -249,7 +249,7 @@ public:
     Iterator& operator--() { Base::decrement(); return *this; }
     // postfix -- intentionally omitted
 
-    // Only forwarding + unsigned is supported.
+    // Only forwarding + size_t is supported.
     Iterator& operator+=(size_t count) { Base::increment(count); return *this; }
     Iterator operator+(size_t count) const { Iterator result(*this); result += count; return result; }
 };
@@ -621,8 +621,8 @@ template<typename T, size_t inlineCapacity>
 template<typename Func>
 inline T Deque<T, inlineCapacity>::takeFirst(NOESCAPE const Func& func)
 {
-    unsigned count = 0;
-    unsigned size = this->size();
+    size_t count = 0;
+    size_t size = this->size();
     while (count < size) {
         T candidate = takeFirst();
         if (func(candidate)) {
@@ -630,7 +630,7 @@ inline T Deque<T, inlineCapacity>::takeFirst(NOESCAPE const Func& func)
                 prepend(takeLast());
             return candidate;
         }
-        count++;
+        ++count;
         append(WTF::move(candidate));
     }
     return T();
@@ -640,8 +640,8 @@ template<typename T, size_t inlineCapacity>
 template<typename Func>
 inline T Deque<T, inlineCapacity>::takeLast(NOESCAPE const Func& func)
 {
-    unsigned count = 0;
-    unsigned size = this->size();
+    size_t count = 0;
+    size_t size = this->size();
     while (count < size) {
         T candidate = takeLast();
         if (func(candidate)) {
@@ -649,7 +649,7 @@ inline T Deque<T, inlineCapacity>::takeLast(NOESCAPE const Func& func)
                 append(takeFirst());
             return candidate;
         }
-        count++;
+        ++count;
         prepend(WTF::move(candidate));
     }
     return T();
