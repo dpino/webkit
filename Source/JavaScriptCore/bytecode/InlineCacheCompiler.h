346454388ab4972d48acfb027fb1079692e51f5c
Adrian Perez de Castro <aperez@igalia.com>
2026-04-15 15:53:59 +0300

    REGRESSION(305877.408@webkitglib/2.52): [JSC] Fix IC prologue sizes for non x86_64 architectures
    Unreviewed.
    
    When backporting 310870@main (a1426e112eee), resolving merge conflicts
    picked the values for "prologueSizeInBytesDataIC" from the the "main"
    branch, which are not correct for the 2.52 stable branch. This patch
    restores the correct values for the branch.
    
    The issue went inadverted due to the value for x86_64 being the same,
    and testing for stable branch backports being done in that architecture.
    
    * Source/JavaScriptCore/bytecode/InlineCacheCompiler.h:
    Restore correct values.
    
    Canonical link: https://commits.webkit.org/305877.433@webkitglib/2.52

--- Source/JavaScriptCore/bytecode/InlineCacheCompiler.h
+++ Source/JavaScriptCore/bytecode/InlineCacheCompiler.h
@@ -528,13 +528,13 @@ bool doesJSCalls(AccessCase::AccessType);
 #if CPU(X86_64)
 static constexpr size_t prologueSizeInBytesDataIC = 1;
 #elif CPU(ARM64E)
-static constexpr size_t prologueSizeInBytesDataIC = 4;
+static constexpr size_t prologueSizeInBytesDataIC = 8;
 #elif CPU(ARM64)
-static constexpr size_t prologueSizeInBytesDataIC = 0;
+static constexpr size_t prologueSizeInBytesDataIC = 4;
 #elif CPU(ARM_THUMB2)
-static constexpr size_t prologueSizeInBytesDataIC = 0;
+static constexpr size_t prologueSizeInBytesDataIC = 6;
 #elif CPU(RISCV64)
-static constexpr size_t prologueSizeInBytesDataIC = 0;
+static constexpr size_t prologueSizeInBytesDataIC = 12;
 #else
 #error "unsupported architecture"
 #endif
