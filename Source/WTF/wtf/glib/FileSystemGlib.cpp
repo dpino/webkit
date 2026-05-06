1ea829ecfcf7acc0504a8a9441eafce60482f312
Thomas Klausner <wiz@gatalith.at>
2026-05-06 00:59:39 -0700

    Cherry-pick 312615@main (4bbc7d631326). https://bugs.webkit.org/show_bug.cgi?id=313992
        Adapt for procfs differences between NetBSD and other operating systems.
        https://bugs.webkit.org/show_bug.cgi?id=313992
    
        Reviewed by Claudio Saavedra.
    
        NetBSD does support procfs, but there are some differences.  One is
        that the link to the executable is not `/proc/curproc/file` but
        `/proc/curproc/exe`.  This causes an error message on the console.
    
        This error also showed an off-by-one - glib2 doesn't like creating
        file names from strings of length zero - fixed as well.
    
        * Source/WTF/wtf/glib/FileSystemGlib.cpp:
        (WTF::FileSystemImpl::currentExecutablePath):
    
        Canonical link: https://commits.webkit.org/312615@main
    
    Canonical link: https://commits.webkit.org/305877.476@webkitglib/2.52

--- Source/WTF/wtf/glib/FileSystemGlib.cpp
+++ Source/WTF/wtf/glib/FileSystemGlib.cpp
@@ -65,7 +65,7 @@ CString currentExecutablePath()
 {
     static char readLinkBuffer[PATH_MAX];
     ssize_t result = readlink("/proc/self/exe", readLinkBuffer, PATH_MAX);
-    if (result == -1)
+    if (result <= 0)
         return { };
 WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN // Linux port
     return CString(std::span { readLinkBuffer, static_cast<size_t>(result) });
@@ -85,16 +85,21 @@ CString currentExecutablePath()
     int selfFd = open("/proc/self/exefile", O_RDONLY);
     ssize_t result = read(selfFd, readBuffer, sizeof(readBuffer));
     close(selfFd);
-    if (result == -1)
+    if (result <= 0)
         return { };
     return CString(unsafeMakeSpan(readBuffer, static_cast<size_t>(result)));
 }
 #elif OS(UNIX)
+#if OS(NETBSD)
+#define _PROC_CURPROC_PATH "/proc/curproc/exe"
+#else
+#define _PROC_CURPROC_PATH "/proc/curproc/file"
+#endif
 CString currentExecutablePath()
 {
     static char readLinkBuffer[PATH_MAX];
-    ssize_t result = readlink("/proc/curproc/file", readLinkBuffer, PATH_MAX);
-    if (result == -1)
+    ssize_t result = readlink(_PROC_CURPROC_PATH, readLinkBuffer, PATH_MAX);
+    if (result <= 0)
         return { };
     return CString(unsafeMakeSpan(readLinkBuffer, static_cast<size_t>(result)));
 }
