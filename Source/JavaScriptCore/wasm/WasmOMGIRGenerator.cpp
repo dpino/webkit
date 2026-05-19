57d041747454b00334c1f11bf417b4e6ecc7c1c9
Shu-yu Guo <syg@apple.com>
2026-05-19 11:33:57 -0700

    Cherry-pick 305413.432@safari-7624-branch (935ddf64e1d2). https://bugs.webkit.org/show_bug.cgi?id=309538
        [JSC] Make wasm name section parsing threadsafe
        https://bugs.webkit.org/show_bug.cgi?id=309538
        rdar://172053974
    
        Reviewed by Yusuke Suzuki.
    
        There is a race when parsing the wasm "name" custom section. Compiler threads
        and the main thread may race on access of the name section.
    
        This PR fixes the race by making the name section go through a rel/acq
        accessor. Lifetime safety is upkept by holding onto the retired name section
        (the initial empty one) when the parser finishes parsing a name section. For
        simplicity, all name sections after the first one are ignored, as there is
        no normative requirement on custom sections.
    
        No test added as manual sleeping is required to widen the window to reproduce.
    
        * Source/JavaScriptCore/wasm/WasmBBQJIT.cpp:
        (JSC::Wasm::BBQJITImpl::BBQJIT::addTopLevel):
        * Source/JavaScriptCore/wasm/WasmCallee.cpp:
        (JSC::Wasm::OptimizingJITCallee::addCodeOrigin):
        * Source/JavaScriptCore/wasm/WasmIPIntPlan.cpp:
        (JSC::Wasm::IPIntPlan::compileFunction):
        * Source/JavaScriptCore/wasm/WasmModuleInformation.cpp:
        (JSC::Wasm::ModuleInformation::setNameSection):
        * Source/JavaScriptCore/wasm/WasmModuleInformation.h:
        * Source/JavaScriptCore/wasm/WasmOMGPlan.cpp:
        (JSC::Wasm::OMGPlan::work):
        * Source/JavaScriptCore/wasm/WasmPlan.cpp:
        (JSC::Wasm::Plan::signpostMessage const):
        * Source/JavaScriptCore/wasm/WasmSectionParser.cpp:
        (JSC::Wasm::SectionParser::parseCustom):
        * Source/JavaScriptCore/wasm/WasmStreamingParser.cpp:
        (JSC::Wasm::StreamingParser::finalize):
    
        Identifier: 305413.432@safari-7624-branch
    
    Canonical link: https://commits.webkit.org/305877.584@webkitglib/2.52

--- Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp
+++ Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp
@@ -6801,7 +6801,7 @@ Expected<std::unique_ptr<InternalFunction>, String> parseAndCompileOMG(Compilati
         ionGraphFunction = JSON::Object::create();
         auto passes = JSON::Array::create();
         ionGraphPasses = passes.get();
-        ionGraphFunction->setString("name"_s, makeString(IndexOrName(functionIndexSpace, info.nameSection->get(functionIndexSpace))));
+        ionGraphFunction->setString("name"_s, makeString(IndexOrName(functionIndexSpace, info.nameSection().get(functionIndexSpace))));
         ionGraphFunction->setArray("passes"_s, WTF::move(passes));
     }
     auto result = makeUnique<InternalFunction>();
